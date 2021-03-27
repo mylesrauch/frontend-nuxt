bd86b62a34a39c926418210709c938aabc7215b7
myles <mylesrauch@gmail.com>
2021-03-26 23:21:10 -0600

    First PUSh!!

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-data-table :headers="headers"
+  :items="logs"
+  :sort-by="created"
+  class="elevation-1"
+  >
+    <template v-slot:top>
+      <v-toolbar
+      
+      >
+        <v-toolbar-title> Logs </v-toolbar-title>
+      </v-toolbar>
+    </template>
+  </v-data-table>
+</template>
---
+++ pages/page.vue
@@ -0,0 +1,13 @@
+<template>
+    <div> <v-btn elevation="2"> !DELETE!</v-btn>  <v-alert type="error" dismissible > cring</v-alert> <v-textarea></v-textarea></div>  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
